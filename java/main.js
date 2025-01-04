0da38a27d6754058cdcb76b22d9523089f639baa
Karmen <karmen.ratiananahary@etu.univ-amu.fr>
2025-01-04 13:53:09 +0100

    Update main.js

--- java/main.js
+++ java/main.js
@@ -10,3 +10,14 @@ function changeTheme(theme) {
     document.getElementById('theme-menu').style.display = 'none';
     closeTheme()
 }
+
+const hautpage = document.querySelector('.hautpage');
+hautpage.addEventListener('click', () =>{
+
+    window.scrollTo({
+        top : 0,
+        left: 0,
+        behavior: "smooth"
+    })
+})
+
