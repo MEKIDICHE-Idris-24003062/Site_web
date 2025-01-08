dbcacda8a1b46c9d5c54867c4dce17f136e73e2b
idris <idris.mekidiche@etu.univ-amu.fr>
2025-01-08 20:45:11 +0100

    Update thème.js

--- JavaScript/thème.js
+++ JavaScript/thème.js
@@ -1,24 +1,17 @@
-// Sélectionne le bouton et le body
-const toggleButton = document.getElementById('toggle-mode');
-const body = document.body;
+function changeTheme() {
+    const link = document.getElementById('theme');
+    const currentTheme = link.href.includes('sombre.css') ? 'sombre.css' : 'index.css';
+    const newTheme = currentTheme === 'sombre.css' ? 'index.css' : 'sombre.css';
 
-// Vérifie si le mode sombre est activé dans le stockage local
-const darkMode = localStorage.getItem('sombre');
-
-if (darkMode === 'enabled') {
-  body.classList.add('sombre');
-  toggleButton.textContent = 'Mode Jour';
+    // Vérifier si on est sur index.html ou une autre page
+    if (window.location.pathname.endsWith("index.html")) {
+        // Comportement pour index.html
+        link.href = `css/${newTheme}`;
+        localStorage.setItem('selected-theme', newTheme);
+    } else {
+        // Comportement pour les autres pages dans HTML/
+        link.href = `../css/${newTheme}`;
+        localStorage.setItem('selected-theme', newTheme);
+        console.log(newTheme)
+    }
 }
-
-// Ajoute un événement de clic pour basculer entre les modes
-toggleButton.addEventListener('click', () => {
-  if (body.classList.contains('sombre')) {
-    body.classList.remove('sombre');
-    toggleButton.textContent = 'Mode Sombre';
-    localStorage.setItem('sombre', 'disabled'); // Enregistre l'état dans LocalStorage
-  } else {
-    body.classList.add('sombre');
-    toggleButton.textContent = 'Mode Jour';
-    localStorage.setItem('sombre', 'enabled'); // Enregistre l'état dans LocalStorage
-  }
-});
